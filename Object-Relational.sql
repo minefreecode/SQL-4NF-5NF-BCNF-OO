e6501bfe5d7c895b890ba353062e0233a17f0ece
aleksey <aleksey.work.job@gmail.com>
2025-03-20 11:41:37 +0300

    [doc] Документирование Задокументированы остальные нормальные формы

--- Object-Relational.sql
+++ Object-Relational.sql
@@ -1,5 +1,7 @@
 /*Example of ORDBMS with oracle*/
 /*force drop all inital tales and types */
+
+/* Удаление всех типов */
 DROP TYPE DRIVERTYPE force;
 DROP TYPE AddressType force;
 DROP TYPE TeamType force;
@@ -9,6 +11,7 @@ DROP TABLE address;
 DROP TYPE TEAMS2 FORCE;
 DROP TABLE TEAMSV2;
 /*begin creating a type and a relevent method some PL/SQL*/
+/*Вводится новый тип*/
 CREATE or replace TYPE DRIVERTYPE as OBJECT
 (
  driver_no NUMBER(2),
@@ -21,7 +24,7 @@ CREATE or replace TYPE DRIVERTYPE as OBJECT
  phone_number VARCHAR2(13),
  MEMBER FUNCTION display_address
      RETURN VARCHAR2);
-/
+/*Вводится еще один тип*/
 CREATE OR REPLACE TYPE BODY DRIVERTYPE AS
 MEMBER FUNCTION display_address
   RETURN VARCHAR2 IS
@@ -29,7 +32,7 @@ BEGIN
 RETURN address.house_no||', '||address.street||', '||address.town||', '||address.postcode||', '||address.country;
 END;
 END;
-/
+/*Создается новый тип как объект */
 CREATE or replace TYPE AddressType as OBJECT
 (
  house_no NUMBER(3),
@@ -38,9 +41,9 @@ CREATE or replace TYPE AddressType as OBJECT
  county VARCHAR2(25),
  postcode VARCHAR2(8),
  country VARCHAR2(3));
-/
+/*Заменяется тип полей */
  CREATE OR REPLACE TYPE telephones IS VARRAY(3) OF VARCHAR2(13);
-/
+/*Создает новый тип как объект. Причем ему принадлежит подтип, который мы определили выше*/
 CREATE TYPE TeamType as OBJECT
 (
  team_no NUMBER(3),
@@ -49,13 +52,13 @@ CREATE TYPE TeamType as OBJECT
  manager REF DRIVERTYPE,
  tel_no telephones
  );
-/
+/*Создается таблица заданного типа. Она связана с таблицей ADDRESS*/
 CREATE TABLE DRIVER OF DRIVERTYPE;
-/
+/*Создается таблица заданного типа*/
 CREATE TABLE TEAM OF TEAMTYPE;
-/
+/*Создается таблица заданного типа. Она связана с таблицей DRIVER*/
 CREATE TABLE ADDRESS OF ADDRESSTYPE;
-/
+/* Вставка данных в саму таблицу и в связную таблицу одноврепменно*/
 INSERT INTO DRIVER VALUES (1,'DANIEL','DW','WILKINSON',AddressType(7,'winner street'
 ,'sunderland','tyne and wear','SR4 8LE','UK'),1,1,'01915347630');
 INSERT INTO DRIVER VALUES (2,'BOB','BM','MARELY',AddressType(49,'looser street'
@@ -69,6 +72,7 @@ INSERT INTO DRIVER VALUES (5,'BUM','BW','WILIAMSON',AddressType(12,'winner stree
 INSERT INTO DRIVER VALUES (6,'MAY','MS','SMITH',AddressType(87,'boo street'
 ,'quabec','a county','CAN 8LE','CAN'),1,1,'01915347635');
 
+/*Ещё вставка с использованием типов и подтипов*/
 INSERT INTO TEAM VALUES (1,'loosers',AddressType(11,'yo street','sunderland','tyne and wear','SR4 EEE','UK'), (SELECT REF(P) from DRIVER P WHERE P.driver_no = 1),telephones('01912345467','01914560986','01915656456'));
 INSERT INTO TEAM VALUES (2,'young',AddressType(22,'me street','newcastle','devon','SR4 BBB','UK'), (SELECT REF(P) from DRIVER P WHERE P.driver_no = 2),telephones('01915347677','01914565435','01915657763'));
 INSERT INTO TEAM VALUES (3,'winners',AddressType(33,'ha street','birmingham','essex','SR4 CCC','UK'), (SELECT REF(P) from DRIVER P WHERE P.driver_no = 3),telephones('01915347388','01913453456','01914556546'));
